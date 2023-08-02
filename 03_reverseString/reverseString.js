a3707a6a2e4f2484fccbf5cc6d02443fe7a75440
labakie <95135910+labakie@users.noreply.github.com>
2023-08-02 21:55:47 +0700

    Add solution for case 1 until 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  const arrWord = word.split("");
+  let reversedWord = "";
+  for (let i = 1; i <= arrWord.length; i++) {
+    reversedWord += arrWord[arrWord.length - i];
+  }
+  return reversedWord;
 };
 
 // Do not edit below this line
