094fda953f09838b464abaae0aaee1bf26ab1d5e
labakie <95135910+labakie@users.noreply.github.com>
2023-08-03 22:21:27 +0700

    Add solution for case 5 until 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return "ERROR";
+  if (firstNumber < 0 || secondNumber < 0) return "ERROR";
+  if (firstNumber < secondNumber) {
+    let total = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        total += i;
+    }
+    return total;
+  } else if (firstNumber > secondNumber) {
+    let total = 0;
+    for (let i = secondNumber; i <= firstNumber; i++) {
+        total += i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const calculateTemp = ((fahrenheit -32) * 5) / 9;
+  return Math.round(calculateTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const calculateTemp = ((celsius * 9) / 5) + 32;
+  return Math.round(calculateTemp * 10) / 10;
 };
 
 // Do not edit below this line
